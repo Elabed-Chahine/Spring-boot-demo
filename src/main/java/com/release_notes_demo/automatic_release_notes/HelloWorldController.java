e5bd16f2803bc685beac53e742eeaa151c3332ff
elabed chahine <elabed.chahine@gmail.com>
2022-08-08 19:50:04 +0100

    fix: Added a Controller file but no code

---
+++ src/main/java/com/release_notes_demo/automatic_release_notes/HelloWorldController.java
@@ -0,0 +1,10 @@
+package com.release_notes_demo.automatic_release_notes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    
+}
